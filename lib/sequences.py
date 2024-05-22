0e3f5796998c225590f52bcda5f49b23853ac110
kevin mayaka <kevinabuga3@gmail.com>
2024-05-22 12:03:33 +0300

    initial commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        return
+    elif length == 1:
+        print(0)
+        return
+    elif length == 2:
+        print(0)
+        print(1)
+        return
+    
+    a, b = 0, 1
+    print(a)
+    print(b)
+    for _ in range(2, length):
+        a, b = b, a + b
+        print(b)
